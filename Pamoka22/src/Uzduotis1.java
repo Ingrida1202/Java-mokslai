1e22962e73854b873557628ab7b41418ec93ff12
Ingrida1202 <danguoleingrida@gmail.com>
2023-06-05 18:46:56 +0300

    java-kintamieji

---
+++ Pamoka22/src/Uzduotis1.java
@@ -0,0 +1,10 @@
+public class Uzduotis1 {
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println("bet koks tekstas");
+        System.out.println("namas");
+        System.out.println(2 + 5);
+        System.out.println("Šiandien yra" + " " + "vasaros pradžia");
+        
+    }
+}
---
+++ Pamoka22/src/Uzduotis2.java
@@ -0,0 +1,18 @@
+public class Uzduotis2 {
+    public static void main(String[] args) {
+        short s = 1538;
+        System.out.println(s);
+        boolean loginisKintamasis = true;
+        System.out.println(loginisKintamasis);
+        String tekstinisKintamasis = "Siandien yra pirmadienis";
+        System.out.println(tekstinisKintamasis);
+        double d = 15565646.32656;
+        System.out.println(d);
+        short x = 125;
+        int y = x;
+        System.out.println(y);
+        int z = 5;
+        byte b = (byte) z;
+        System.out.println(b);
+    }
+}
