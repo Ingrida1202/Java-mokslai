cc6c50bbf2bf53deed6c79492f679d6309103d51
Ingrida1202 <danguoleingrida@gmail.com>
2023-06-08 20:10:16 +0300

    funkcijaTrueFalse

--- Pamoka23/src/Uzduotis3.java
+++ Pamoka23/src/Uzduotis3.java
@@ -1,5 +1,10 @@
 public class Uzduotis3 {
     public static void main(String[] args) {
-        
+        System.out.println(arKartotinis(20, 5));
+        System.out.println(arKartotinis(14, 5));
+    }
+    
+    public static boolean arKartotinis(int x, int y) {
+        return x % y == 0;
     }
 }
